8760465d24cdfc695fa5246baf45b55506ca3db8
Abraham Green <Evergreen1504@gmail.com>
2023-02-05 22:34:42 -0500

    Implementación de MVC funcionando

--- Controlador/products.js
+++ Controlador/products.js
@@ -7,4 +7,52 @@ const getProducts = (req, res) => {
         else console.log(err);
     })};
 
-module.exports = {getProducts};
+const getProduct = (req, res) => {
+    sqlConnection.query(
+        "SELECT * FROM product WHERE id = ?",
+        [req.params.id],
+        (err, rows, fields) => {
+        if (!err) res.send(rows);
+        else console.log(err);
+})};
+
+const createProduct = (req, res) => {
+    let product = req.body;
+        if(product.id === undefined){
+            product.id = 0;
+        }
+        var sql ='Insert into product values ('+ product.id +',"'+ product.nombre +'",'+ product.precio +')';
+        sqlConnection.query(
+            sql,
+            (err, rows, fields) => {
+            if (!err) res.send("Product Created Successfully");
+            else console.log(err);
+            }
+        );
+    }
+
+const updateProduct = (req, res) => {
+    let product = req.body;
+    var sql = 'UPDATE product SET nombre = "'+product.nombre +'", precio = '+ product.precio+' WHERE id = ?'; 
+        sqlConnection.query(
+        sql,
+        [req.params.id],
+        (err, rows, fields) => {
+        if (!err) res.send("Product Details Updated Successfully");
+        else console.log(err);
+        }
+    );
+};
+
+const deleteProduct = (req, res) => {
+    sqlConnection.query(
+                "DELETE FROM product WHERE id = ?",
+                [req.params.id],
+                (err, rows, fields) => {
+                if (!err) res.send("Product Record deleted successfully.");
+                else console.log(err);
+                }
+            );
+};
+
+module.exports = {getProducts, getProduct, createProduct, updateProduct, deleteProduct};
--- Rutas/productos.js
+++ Rutas/productos.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const rutas = express.Router();
-const {getProducts} = require('../Controlador/products') 
+const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require('../Controlador/products') 
 
 // Todo el CRUD va aquí dentro 
 
 rutas.get("/products", getProducts);
+rutas.get("/product/:id", getProduct);
+rutas.post("/product", createProduct);
+rutas.put("/product/:id", updateProduct);
+rutas.delete("/product/:id", deleteProduct);
 
 
 module.exports = rutas
